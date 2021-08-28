0f5e88cfc492b32bd3385f7f7667aedf59622726
chekwei4 <chekwei@me.com>
2021-08-28 22:00:20 +0800

    added deployment files

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.experimental import enable_iterative_imputer
